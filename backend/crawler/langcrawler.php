6227a9a8506f0b15be55bf82c6fb6fcbc8af1a23
Anika <Anika.Westburg@hhu.de>
2013-05-26 18:38:00 +0200

    neuer languagecrawler und API von detectlanguage (Crawler noch nicht getestet)

--- backend/crawler/langcrawler.php
+++ backend/crawler/langcrawler.php
@@ -5,17 +5,41 @@
   if (!$db) echo "Beim Zugriff auf die Datenbank ist ein Fehler aufgetreten. Bitte versuchen Sie es sp&auml;ter nochmal.<br/><br/>";
   else {
   	require_once 'detectlanguage.php';
-  	$detectlanguage = new DetectLanguage("YOUR API KEY");
+  	$detectlanguage = new DetectLanguage("YOUR API KEY"); //Absicht
   	if ((isset($argv[1])) AND $argv[1]!="" ){
   		$zyklus=$argv[1];
   	} else $zyklus = 1;
   	if ((isset($argv[2])) AND $argv[2]!="" ){
   		$steps=$argv[2];
   	} else $steps = 1;  
-  	$query = "SELECT * FROM";
-  	//Abfrage: alle eingelesenen Texte mit Volltexten ohne language gesetzt
-  	//$steps-viele Texte lesen und in Abfrage an detectlanguage senden
-  	//erhaltene Sprachen in DB schreiben 	
-  	
+  	$query = "SELECT * FROM `$database`.$dokument WHERE $full_text !='' AND $full_text is not null AND $language !='' AND $language is not null;";
+  	$result = mysql_query($query,$connection);
+  	if(!$result) {
+  		print "Fehler: " . mysql_error($connection);
+  	} else {
+  	  for ($i=0; $i<$zyklus; $i++) {
+  	  	$texts = null;
+  	  	for ($j=0; $j<$steps && $row = mysql_fetch_array($result,MYSQL_ASSOC); $j++) {
+  	  		$fulltext = $row[$full_text];
+  	  		$dids[$j] = $row[$id_dokument];
+  	  		$fulltext1 = explode(" ",$fulltext); 
+  	  		$fulltext = "";
+  	  		for ($k=0; $k<10; $k++) {
+  	  			$fulltext += $fulltext1[$k];
+  	  		}
+            $texts[$j] = $fulltext;  	  		
+  	  	}
+  	  	$results = $detectlanguage->detect($texts);
+  	    for ($j=0; $j<$steps; $j++) {
+  	    	$query2 = "UPDATE `$database`.$dokument SET $language='$results[$j][0]' WHERE $id_dokument=$dids[$j];";
+  	    	$result2 = mysql_query($query2,$connection);
+  	    	if(!$result2) {
+  	    		print "Fehler: " . mysql_error($connection);
+  	    	} else {
+  	    		//Erfolg
+  	    	}
+  	    }
+  	  }	
+  	}	  	
   }
 ?>
