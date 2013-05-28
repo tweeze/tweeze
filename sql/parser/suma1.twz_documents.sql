1b6c2f62fe3c5c6d9f135fd3f9decbbd40068cce
Simon <patrick.simon@hhu.de>
2013-05-29 00:33:29 +0200

    Angepasstes DB-Model -> Parser/Resolver
    Signed-off-by: Simon <patrick.simon@hhu.de>

--- sql/parser/suma1.twz_documents.sql
+++ sql/parser/suma1.twz_documents.sql
@@ -14,11 +14,11 @@ grant all on suma1.* to 'suma1'@'localhost' identified by 'suma1' with grant opt
 # C. set database to suma1:
 use suma1;
 
-# D. import tables/data:
-# mysqladmin -u root -p suma1 < /SQL_DUMP.SQL
+# D. disable constraint checks
+set FOREIGN_KEY_CHECKS=0;
 
-# E. disable constraint checks
-# set FOREIGN_KEY_CHECKS=0;
+# E. import tables/data:
+# mysqladmin -u root -p suma1 < /SQL_DUMP.SQL
 
 # CREATE TABLES:
 
@@ -27,13 +27,16 @@ use suma1;
 drop table if exists suma1.twz_documents;
 create table if not exists suma1.twz_documents (
 id bigint(20) unsigned not null auto_increment,
-urls_id bigint(20) unsigned,
+urls_final_id bigint(20) unsigned,
 parsed boolean default false,
 parse_date datetime default null,
 full_text mediumtext default null,
 meta_desc text default null,
 meta_keyword text default null,
-index urls_id_idx (urls_id),
+index urls_final_id_idx (urls_final_id),
 primary key (id),
-foreign key (urls_id) references suma1.twz_urls(id) on update cascade on delete cascade
+foreign key (urls_final_id) references suma1.twz_urls_final(id) on update cascade on delete cascade
 ) engine=InnoDB default charset utf8;
+
+# F. enable constraint checks
+set FOREIGN_KEY_CHECKS=1;
