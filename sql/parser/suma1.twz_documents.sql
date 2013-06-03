75f8b3bd14f5af2d4148c1045073f4e250456b19
Simon <patrick.simon@hhu.de>
2013-06-03 23:08:34 +0200

    Update -> suma1.twz_documents
    Signed-off-by: Simon <patrick.simon@hhu.de>

--- sql/parser/suma1.twz_documents.sql
+++ sql/parser/suma1.twz_documents.sql
@@ -1,28 +1,29 @@
-# TWEEZE
-# MYSQL DATABASE “suma1”
-# > suma1.twz_documents.stmts.sql
+# SQL
+# 
+# DB: MySQL
+# Database: suma1
+# File: suma1.twz_documents.sql
 
-# PREQUISITE:
+# PREREQUISITES:  
 
-# A. create database suma1:
-
+# A. Create database suma1:
 create database if not exists suma1 default character set uft8;
 
-# B. grant privileges:
+# B. Grant privileges to user 'suma1':
 grant all on suma1.* to 'suma1'@'localhost' identified by 'suma1' with grant option;
 
-# C. set database to suma1:
+# C. Set current database to 'suma1':
 use suma1;
 
-# D. disable constraint checks
-set FOREIGN_KEY_CHECKS=0;
+# D. Temporarly disable constraint checks:
+set foreign_key_checks=0;
 
-# E. import tables/data:
-# mysqladmin -u root -p suma1 < /SQL_DUMP.SQL
+# E. Import tables/data:
+# > mysqladmin -u root -p suma1 < /%SQLDUMP%.SQL
 
-# CREATE TABLES:
+# SQL STATEMENTS:
 
-# 1. drop/create table suma1.twz_documents:
+# 1. Drop/Create table 'suma1.twz_documents':
 
 drop table if exists suma1.twz_documents;
 create table if not exists suma1.twz_documents (
@@ -40,4 +41,4 @@ foreign key (urls_final_id) references suma1.twz_urls_final(id) on update cascad
 ) engine=InnoDB default charset utf8;
 
 # F. enable constraint checks
-set FOREIGN_KEY_CHECKS=1;
+set foreign_key_checks=1;
